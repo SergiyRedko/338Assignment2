5f400ab9d16d8191d212b523bf1a10ad3f444849
TurtwigPlayz <63518111+TurtwigPlayz@users.noreply.github.com>
2023-02-10 23:13:11 -0700

    Exercise 2

---
+++ ex2.2.py
@@ -0,0 +1,68 @@
+import json
+import multiprocessing
+import sys
+import time
+
+import matplotlib.pyplot as plt
+
+sys.setrecursionlimit(20000)
+def func1(arr, low, high):
+    if low < high:
+        pi = func2(arr, low, high)
+        func1(arr, low, pi-1)
+        func1(arr, pi + 1, high)
+def func2(array, start, end):
+    p = array[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and array[high] >= p:
+            high = high - 1
+        while low <= high and array[low] <= p:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+    return high
+
+def sort_array(array):
+    func1(array, 0, len(array)-1)
+    return array
+
+def split_array(array, chunk_size):
+    return [array[i:i+chunk_size] for i in range(0, len(array), chunk_size)]
+
+def divide_and_conquer(array, chunk_size):
+    chunks = split_array(array, chunk_size)
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+    sorted_chunks = pool.map(sort_array, chunks)
+    pool.close()
+    pool.join()
+    return sorted(sorted_chunks)
+
+
+if __name__ == '__main__':
+    with open('ex2.5.json', "r") as f:
+        data = json.load(f)
+
+    times = []
+    arr_sizes = []
+
+    for arr in data:
+
+        start_time = time.time()
+        divide_and_conquer(arr, chunk_size=1000)
+        stop_time = time.time()
+        execution_time = stop_time - start_time
+        arr_sizes.append(len(arr))
+        times.append(execution_time)
+
+
+
+    plt.plot(arr_sizes, times)
+    plt.xlabel("Array Size")
+    plt.ylabel("Execution Time (s)")
+    plt.title("Sorting Preformance")
+    plt.show()
---
+++ ex2.4.py
@@ -0,0 +1,72 @@
+import json
+import random
+import sys
+import time
+
+import matplotlib.pyplot as plt
+
+sys.setrecursionlimit(20000)
+
+def func1(arr, low, high):
+    # Terminate the recursion if the sub-array size is small
+    if high - low <= 10:
+        func2(arr, low, high)
+        return
+    
+    # Choose the pivot randomly
+    pivot_index = random.randint(low, high)
+    arr[low], arr[pivot_index] = arr[pivot_index], arr[low]
+    pivot = arr[low]
+
+    i = low + 1
+    j = high
+
+    while True:
+        while i <= j and arr[i] <= pivot:
+            i += 1
+        while i <= j and arr[j] >= pivot:
+            j -= 1
+        if i <= j:
+            arr[i], arr[j] = arr[j], arr[i]
+        else:
+            break
+
+    arr[low], arr[j] = arr[j], arr[low]
+
+    # Tail call optimization
+    func1(arr, low, j - 1)
+    func1(arr, j + 1, high)
+
+def func2(arr, low, high):
+    for i in range(low + 1, high + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= low and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+if __name__ == '__main__':
+    with open('ex2w.json', "r") as f:
+        data = json.load(f)
+
+    times = []
+    arr_sizes = []
+
+    for arr in data:
+
+        start_time = time.time()
+        func1(arr, 0, len(arr) - 1)
+        stop_time = time.time()
+        execution_time = stop_time - start_time
+        arr_sizes.append(len(arr))
+        times.append(execution_time)
+
+
+
+    plt.plot(arr_sizes, times)
+    plt.xlabel("Array Size")
+    plt.ylabel("Execution Time (s)")
+    plt.title("Sorting Preformance")
+    plt.show()
